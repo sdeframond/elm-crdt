f81f32c77e38b8bef5b244b178e0785f2a07707e
Samuel DE FRAMOND <samuel@framond.fr>
2022-07-16 16:45:21 +0200

    Implement UndoList

---
+++ src/UndoList.elm
@@ -0,0 +1,71 @@
+module UndoList exposing
+    ( UndoList
+    , do
+    , init
+    , redo
+    , undo
+    , value
+    )
+
+
+type UndoList a op
+    = UL
+        { undo : List op
+        , redo : List op
+        , value : a
+        }
+
+
+init : a -> UndoList a op
+init a =
+    UL
+        { undo = []
+        , redo = []
+        , value = a
+        }
+
+
+value : UndoList a op -> a
+value (UL data) =
+    data.value
+
+
+do : (op -> a -> b) -> op -> UndoList a op -> UndoList b op
+do apply op (UL data) =
+    UL
+        { value = apply op data.value
+        , undo = op :: data.undo
+        , redo = data.redo
+        }
+
+
+undo : (op -> a -> b) -> UndoList a op -> Maybe (UndoList b op)
+undo unapply (UL data) =
+    case data.undo of
+        [] ->
+            Nothing
+
+        prevOp :: rest ->
+            Just
+                (UL
+                    { value = unapply prevOp data.value
+                    , undo = rest
+                    , redo = prevOp :: data.redo
+                    }
+                )
+
+
+redo : (op -> a -> b) -> UndoList a op -> Maybe (UndoList b op)
+redo apply (UL data) =
+    case data.redo of
+        [] ->
+            Nothing
+
+        nextOp :: rest ->
+            Just
+                (UL
+                    { value = apply nextOp data.value
+                    , undo = nextOp :: data.undo
+                    , redo = rest
+                    }
+                )
---
+++ tests/UndoListTest.elm
@@ -0,0 +1,70 @@
+module UndoListTest exposing (suite)
+
+import Expect
+import Fuzz exposing (Fuzzer)
+import Test exposing (..)
+import UndoList
+
+
+type alias Op =
+    Int
+
+
+apply : Op -> Int -> Int
+apply op i =
+    i + op
+
+
+unapply : Op -> Int -> Int
+unapply op i =
+    i - op
+
+
+operation : Fuzzer Op
+operation =
+    Fuzz.int
+
+
+undoList : Fuzzer (UndoList.UndoList Int Op)
+undoList =
+    let
+        actions =
+            [ operation |> Fuzz.map (UndoList.do apply)
+            , (\ul -> UndoList.undo unapply ul |> Maybe.withDefault ul) |> Fuzz.constant
+            , (\ul -> UndoList.redo apply ul |> Maybe.withDefault ul) |> Fuzz.constant
+            ]
+
+        applyOps ops =
+            List.foldr (\f x -> f x) (UndoList.init 0) ops
+    in
+    actions |> Fuzz.oneOf |> Fuzz.list |> Fuzz.map applyOps
+
+
+suite : Test
+suite =
+    describe "UndoList"
+        [ test ".value returns the value" <|
+            \_ ->
+                UndoList.init 1 |> UndoList.value |> Expect.equal 1
+        , fuzz2 undoList operation ".do applies the operation" <|
+            \ul op ->
+                UndoList.do apply op ul
+                    |> UndoList.value
+                    |> Expect.equal (op + UndoList.value ul)
+        , fuzz2 undoList operation ".undo restores the previous state" <|
+            \ul op ->
+                UndoList.do apply op ul
+                    |> UndoList.undo unapply
+                    |> Maybe.map UndoList.value
+                    |> Expect.equal (Just <| UndoList.value ul)
+        , fuzz2 undoList operation ".redo restores the next state" <|
+            \ul op ->
+                let
+                    next =
+                        UndoList.do apply op ul
+                in
+                UndoList.undo unapply next
+                    |> Maybe.andThen (UndoList.redo apply)
+                    |> Maybe.map UndoList.value
+                    |> Expect.equal (Just <| UndoList.value next)
+        ]
